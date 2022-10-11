c1c957c2bb7a430b72c3c842f42f5c29aab9bf89
dungpt24082002 <dungpt24082002@gmail.com>
2022-10-11 17:55:16 +0700

    dua bai1.c len github

---
+++ bai1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+#define PI 3.1416   
+int main(){
+    float r;
+    printf("Nhap ban kinh duong tron: ");
+    scanf("%f", &r);
+    float s,cv;
+    s=PI * r * r;
+    cv=2*PI*r;
+    printf("Chu vi duong tron la: %f\n",cv);
+    printf("Dien tich hinh tron la: %f", s);
+    return 0;
+}
